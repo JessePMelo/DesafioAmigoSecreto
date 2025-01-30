aa8d709ef9269ffd3c04df6f1052256970e20299
Jesse Melo <jesse_pereira_melo@live.com>
2025-01-29 23:31:02 -0300

    Ajustando insercao de nomes de acordo com o trello

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let nomes = [];
-
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
-    if(nome==""){
-        alert("Nome não pode estar em branco");
-    }
-    else{
-        nomes.push(nome);
+    if (nome == "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        let listaDeNomes = document.getElementById('listaAmigos');
+        let nomesNaLista = listaDeNomes.getElementsByTagName('li');
+        let nomesArray = Array.from(nomesNaLista);
+        let nomeDuplicado = false;
+        
+        for (let i = 0; i < nomesArray.length; i++) {
+            if (nomesNaLista[i].textContent == nome) {
+                nomeDuplicado = true;
+                break;
+            }
+        }
+
+        if (nomeDuplicado) {
+            alert('Nome Duplicado');
+        } else {
+            listaDeNomes.innerHTML += `<li>${nome}</li>`;
+        }
+
         document.getElementById('amigo').value = "";
-        console.log(nomes);
     }
-    
 }
+
 
 function sortearAmigo() {
     let numeroEscolhido = parseInt(Math.random() * nomes.length + 1);
     document.getElementById('paragrafo').innerHTML = `<p>O Amigo secreto sorteado é ${nomes[numeroEscolhido-1]}</p>`;
 }
+
+
+document.getElementById('listaAmigos').innerHTML += `<li>${nome}</li>`;
+document.getElementById('amigo').value = "";
