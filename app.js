072e1c2ecafc2bce8b56047f94273e1d53e71978
Jesse Melo <jesse_pereira_melo@live.com>
2025-01-29 22:04:59 -0300

    Adicionar Funcoes adicionar amigo e sortear app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('nome').value;
+    if(nome==""){
+        alert("Nome não pode estar em branco");
+    }
+    else{
+        nomes.push(nome);
+        document.getElementById('nome').value = "";
+        console.log(nomes);
+    }
+    
+}
+
+function sortearAmigo() {
+    let numeroEscolhido = parseInt(Math.random() * nomes.length + 1);
+    document.getElementById('paragrafo').innerHTML = `<p>O Amigo secreto sorteado é ${nomes[numeroEscolhido-1]}</p>`;
+}
