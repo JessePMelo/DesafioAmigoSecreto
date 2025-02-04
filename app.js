2a5cf59e465276655e76238ad84864f833a4e633
Jesse Melo <jesse_pereira_melo@live.com>
2025-02-03 21:10:44 -0300

    Ajustado todas funcionalidades

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema. 
+let nomesNaLista = [];
+
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
     if (nome == "") {
         alert("Por favor, insira um nome.");
     } else {
         let listaDeNomes = document.getElementById('listaAmigos');
-        let nomesNaLista = listaDeNomes.getElementsByTagName('li');
-        let nomesArray = Array.from(nomesNaLista);
+        nomesNaLista = listaDeNomes.getElementsByTagName('li');
         let nomeDuplicado = false;
         
-        for (let i = 0; i < nomesArray.length; i++) {
+        for (let i = 0; i < nomesNaLista.length; i++) {
             if (nomesNaLista[i].textContent == nome) {
                 nomeDuplicado = true;
                 break;
@@ -26,10 +27,21 @@ function adicionarAmigo() {
     }
 }
 
-
 function sortearAmigo() {
-    if (nomesArray.length > 1){
-        let numeroEscolhido = parseInt(Math.random() * nomesArray.length + 1);
-        document.getElementById('paragrafo').innerHTML = `<p>O Amigo secreto sorteado é ${nomes[numeroEscolhido]}</p>`;    
-    }  
+    if (nomesNaLista.length > 2){
+        let numeroEscolhido = parseInt(Math.random() * nomesNaLista.length);
+        let amigoSorteado = nomesNaLista[numeroEscolhido].textContent ;
+        
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = `A Pessoa sorteada é: ${amigoSorteado}`;
+
+        listaDeNomes = document.getElementById('listaAmigos');
+        listaDeNomes.parentNode.replaceChild(paragrafo, listaDeNomes);
+        
+        console.log(amigoSorteado);
+    }
+    else{
+        alert('Precisa de 3 nomes para o amigo secreto') ;
+    }
+    
 }
